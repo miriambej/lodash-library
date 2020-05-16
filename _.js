95196bbbef94541df3a8ca7c1885bfc2ca9c394d
miriambej <justme0112@hotmail.com>
2020-05-16 17:41:07 +1000

    add comments

--- _.js
+++ _.js
@@ -1,10 +1,12 @@
 
 const _ = {
+  //Clamps number within the inclusive lower and upper bounds.
   clamp(number, lower, upper) {
     const lowerClampedValue = Math.max(number, lower);
     const clampedValue = Math.min(lowerClampedValue, upper);
     return clampedValue;
-  },
+
+  //Checks if number is between start and up to, but not including, end. If end is not specified, it's set to start with start then set to 0. If start is greater than end the params are swapped to support negative ranges.
   inRange(number, start, end) {
     if(end === undefined) {
       end = start;
@@ -25,10 +27,12 @@ const _ = {
        return true;
     }
   },
+  //Splits string into an array of its words.
   words(string) {
     const words = string.split(" ");
     return words
   },
+  //Pads string on the left and right sides if it's shorter than length. Padding characters are truncated if they can't be evenly divided by length.
   pad(string, length) {
     if (length <= string.length) {return string};
     const startPaddingLength = Math.floor((length - string.length) / 2);
@@ -36,6 +40,7 @@ const _ = {
     const paddedString = ' '.repeat(startPaddingLength) + string + ' '.repeat(endPaddingLength);
     return paddedString;
   },
+  //checks to see if the provided object contains a value at the specified key,  return true if the object contains a value at the key and will return false if not.
   has(object, key) {
     if(object.hasOwnProperty(key)) {
       return true;
@@ -78,7 +83,7 @@ const _ = {
     let droppedArray = this.drop(array, dropNumber);
     return droppedArray;
   }
-  //Creates an array of elements split into groups the length of size. If array can't be split evenly, the final chunk will be the remaining elements.
+  
 
 };
 
